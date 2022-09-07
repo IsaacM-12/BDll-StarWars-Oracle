ca489c97df3ef240df1992a3743e08ead3e95846
Isaac <isaacgatgens13@gmail.com>
2022-09-06 20:42:57 -0600

    nada importante

--- BDStarWars/src/main/java/com/BDll/BDStarWars/controller/PersonajeController.java
+++ BDStarWars/src/main/java/com/BDll/BDStarWars/controller/PersonajeController.java
@@ -3,6 +3,7 @@ package com.BDll.BDStarWars.controller;
 import com.BDll.BDStarWars.models.Personaje;
 import com.BDll.BDStarWars.repository.PersonajeRepository;
 import com.BDll.BDStarWars.service.PersonajeService;
+import org.apache.coyote.Request;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,19 +19,19 @@ public class PersonajeController {
     @Autowired
     private PersonajeRepository personajeRepository;
 
-//    private final PersonajeService personajeService;
-//
-//    @Autowired
-//    public PersonajeController(PersonajeService personajeService) {
-//        this.personajeService = personajeService;
-//    }
+    private final PersonajeService personajeService;
+
+    @Autowired
+    public PersonajeController(PersonajeService personajeService) {
+        this.personajeService = personajeService;
+    }
 
     @GetMapping(path = "/personajes")
     public List<Personaje> getPersonaje() {
         return personajeRepository.findAll();
     }
 
-    @GetMapping(path = "/personajes/id'")
+    @GetMapping(path = "/personajes/:personaje-id'")
     public Personaje getById() {
         return null; //personajeRepository.existsById();
     }
