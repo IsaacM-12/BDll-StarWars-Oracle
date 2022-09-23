dae0ff78a53f79c0837e4db02d434c08231a2d3a
Isaac <isaacgatgens13@gmail.com>
2022-09-22 21:43:47 -0600

    falta llamar el procedure

--- BDStarWars/src/main/java/com/BDll/BDStarWars/service/RelationService.java
+++ BDStarWars/src/main/java/com/BDll/BDStarWars/service/RelationService.java
@@ -3,6 +3,7 @@ package com.BDll.BDStarWars.service;
 import com.BDll.BDStarWars.interfaces.IRelation;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.StoredProcedureParameter;
 import javax.swing.*;
 import java.sql.CallableStatement;
 
@@ -12,16 +13,17 @@ public class RelationService implements IRelation {
 
     @Override
     public void insertPersonaje(int idPer, int idPlace, int idType) {   // no sirve
-        String sql = "{call insertarRelacion(124,32,23)}";
+        String sql = "{call insertarRelacion(?,?,?)}";
 
         try {
             CallableStatement prepareCall = getConnection().prepareCall(sql);
+            System.out.println(idPer);
 
-//            prepareStatement.setInt(1, idPer);
-//            prepareStatement.setInt(2, idPlace);
-//            prepareStatement.setInt(3, idType);
-            prepareCall.execute();
-           // prepareCall.close();
+            prepareCall.setInt("idPersonaje2", idPer);
+            prepareCall.setInt("idType", idPlace);
+            prepareCall.setInt("idPlace", idType);
+            prepareCall.executeQuery();
+            prepareCall.close();
 
             } catch (Exception e){
             JOptionPane.showMessageDialog(null,"No se pudo insertar");
