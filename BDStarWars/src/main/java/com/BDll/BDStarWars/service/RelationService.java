bd2ad0f06d8cf44f12aa985c20efb439d66560cb
Isaac <isaacgatgens13@gmail.com>
2022-09-23 19:55:58 -0600

    procedure llamado exitosamente

--- BDStarWars/src/main/java/com/BDll/BDStarWars/service/RelationService.java
+++ BDStarWars/src/main/java/com/BDll/BDStarWars/service/RelationService.java
@@ -1,73 +1,26 @@
 package com.BDll.BDStarWars.service;
 
 import com.BDll.BDStarWars.interfaces.IRelation;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Service;
+import javax.persistence.*;
 
-import javax.swing.*;
-import java.sql.CallableStatement;
-import static com.BDll.BDStarWars.Conection.OracleConexion.getConnection;
-
-// ni se ejecuta el procedure
-
-@SpringBootApplication
+@Service
 public class RelationService implements IRelation {
 
+    @PersistenceContext
+    EntityManager entityManager;
+
+    //String sql = "{ call insertar_Relacion(?,?,?) }";
     @Override
     public void insertPersonaje(int idPer, int idPlace, int idType) {
-        String sql = "{ call insertar_Relacion(?,?,?) }";
-
-        try {
-            var con =getConnection();
-            CallableStatement prepareCall = con.prepareCall(sql);
-
-            prepareCall.setInt(1, idPer);
-            prepareCall.setInt(2, idPlace);
-            prepareCall.setInt(3, idType);
-            prepareCall.execute();
-            prepareCall.close();
-
-        } catch (Exception e){
-            JOptionPane.showMessageDialog(null,"No se pudo insertar");
-        }
+        StoredProcedureQuery query = entityManager
+                .createStoredProcedureQuery("insertar_Relacion")
+                .registerStoredProcedureParameter("idPersonaje2",Integer.class, ParameterMode.IN)
+                .registerStoredProcedureParameter("idType",Integer.class, ParameterMode.IN)
+                .registerStoredProcedureParameter("idPlace",Integer.class, ParameterMode.IN)
+                .setParameter("idPersonaje2", idPer)
+                .setParameter("idType", idType)
+                .setParameter("idPlace", idPlace);
+        query.execute();
     }
 }
-
-
-////    private SessionFactory sessionFactory = HibernateUtils.getHibernateQuery();
-////
-////    @Override
-////    public void insertPersonaje(int idPer, int idPlace, int idType) {   // no sirve
-////        String sql = "{call insertarRelacion(987,87,67)}";
-////
-////        try {
-////            if (!sessionFactory.getCurrentSession().getTransaction().isActive())
-////                sessionFactory.getCurrentSession().getTransaction().begin();
-////            SQLQuery sqlQuery = sessionFactory.getCurrentSession()
-////                    .createSQLQuery(sql);
-////        } catch (Exception e){
-////            JOptionPane.showMessageDialog(null,"No se pudo insertar");
-////        }
-////    }
-//}
-
-
-//
-//@SpringBootApplication
-//public class RelacionSer implements CommandLineRunner {
-//
-//    private JdbcTemplate jdbcTemplate;
-//
-//    @Override
-//    public void run(String... args) throws Exception {   // no sirve
-//        String sql = "{ call insertarRelacion(?,?,?) }";
-//
-//        try {
-//            jdbcTemplate.update(sql, 12, 12, 13);
-//
-//        } catch (Exception e){
-//            JOptionPane.showMessageDialog(null,"No se pudo insertar");
-//        }
-//    }
-//
-//}
-
